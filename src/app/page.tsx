569ae37ebfb699f17e1a9b815ba35cfedd7aa378
Zeke Abshire <zekeabshire@gmail.com>
2024-06-23 17:34:06 -0500

    Added assets + packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,56 @@
-import Link from "next/link";
+import Image from "next/image";
+import { cn } from "~/lib/utils";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+    <main className="">
+      <Hero />
+      <Sponsor />
+      <Flyer />
+      <Endorsements />
+      <Afterparty />
     </main>
   );
 }
+
+const Hero = () => {
+  return (
+    <section>
+      <h1>Jetlife Carshow</h1>
+      <Poster src="/assets/flyers/jetlife-carshow.jpg" />
+      <Poster src="/assets/flyers/dream-doll.jpg" />
+    </section>
+  );
+};
+
+const Sponsor = () => {
+  return <section></section>;
+};
+
+const Flyer = () => {
+  return <section></section>;
+};
+
+const Endorsements = () => {
+  return <section></section>;
+};
+
+const Afterparty = () => {
+  return <section></section>;
+};
+
+const Poster = ({
+  src,
+  alt = "",
+  className,
+}: {
+  src: string;
+  alt?: string;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("relative size-60", className)}>
+      <Image src={src} alt={alt} className="object-contain" fill />
+    </div>
+  );
+};
